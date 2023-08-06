f5b6f113f3956688051dfbe520600ddd5add5204
mustafaelhdad <Mostafa.eng_2194@eng.kfs.edu.eg>
2023-08-06 09:25:12 +0300

    ...

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,21 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Form() {
+  const [description, setDescription] = useState("")
+  const [quantity, setQuantity] = useState(0)
+
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log(e)
+
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() }
+    console.log(newItem)
+
+    setDescription("")
+    setQuantity(0)
   }
 
   return (
     <form className='add-form' onSubmit={ handleSubmit }>
       <h3>What do you need for your trip?</h3>
 
-      <select name="" id="">
+      <select value={ quantity } onChange={ (e) => setQuantity(+e.target.value) }>
         { Array.from({ length: 20 }, (_, i) => i + 1).map(num =>
           <option value={ num } key={ num }>{ num }</option>) }
       </select>
 
-      <input type="text" placeholder='Item...' />
+      <input type="text" placeholder='Item...' value={ description } onChange={ (e) => setDescription(e.target.value) } />
       <button>Add</button>
     </form>
   )
