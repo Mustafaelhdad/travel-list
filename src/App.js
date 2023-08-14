ea45b02bbd1f5fe17a2041c8f296143e6ebe4150
mustafaelhdad <Mostafa.eng_2194@eng.kfs.edu.eg>
2023-08-14 09:29:16 +0300

    ...

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import Stats from './components/Stats'
 import { useState } from 'react'
 
 const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 3, description: "Screens", quantity: 44, packed: true },
+  // { id: 1, description: "Passports", quantity: 2, packed: false },
+  // { id: 2, description: "Socks", quantity: 12, packed: false },
+  // { id: 3, description: "Screens", quantity: 44, packed: false },
 ];
 
 function App() {
@@ -30,7 +30,7 @@ function App() {
       <Logo />
       <Form onAddItems={ handleAddItem } />
       <PackingList items={ items } onDeleteItem={ handleDeleteItem } handleToggleItem={ handleToggleItem } />
-      <Stats />
+      <Stats items={ items } />
     </div>
   );
 }
